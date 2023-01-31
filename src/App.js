49b748ff572e66029a3ddf0aba9e16d77b95f3a0
divyanshukumarsingh <divyanshu.12020666@lpu.in>
2023-01-31 18:16:10 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import TodoList from './component/TodoList';
+
+import React, { useState } from 'react'
+
 function App() {
+
+  const [show, setShow] = useState(true)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className="btn btn-primary" onClick={() => setShow(!show)}>Hide</button>
+      {show ? <TodoList /> : <div>Good Bye</div>}
+
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+
+
+
